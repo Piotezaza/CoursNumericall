3e4a49b64246495c067752f4f7602c19a263ed8f
Piotezaza <mazarine.h@gmail.com>
2018-06-14 14:54:01 +0200

    Update

--- WordPress/Fichier theme perso/index.php
+++ WordPress/Fichier theme perso/index.php
@@ -11,7 +11,7 @@
 	<link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed:400,700|Roboto+Slab:400,700" rel="stylesheet">
 	
 	<!-- mes styles -->
-	<link rel="stylesheet" href="<?php get_template_directory_uri() ?>/css/styles.css">
+	<link rel="stylesheet" href="<?= get_template_directory_uri() ?>/css/styles.css">
 
 </head>
 <body>
@@ -83,21 +83,21 @@
 			<h3>Our latest works</h3>
 			<div class="container">
 				<article class="col">
-					<img src="img/image1.jpg" alt="Business Card">
+					<img src="<?= get_template_directory_uri() ?>/img/image1.jpg" alt="Business Card">
 					<h4>Nobis Business Card</h4>
 					<h5>Business Cards, Graphics</h5>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
 				</article>
 
 				<article class="col">
-					<img src="img/image2.jpg" alt="New fun project">
+					<img src="<?= get_template_directory_uri() ?>/img/image2.jpg" alt="New fun project">
 					<h4>New fun project</h4>
 					<h5>Webdesign, Application</h5>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
 				</article>
 				
 				<article class="col">
-					<img src="img/image3.jpg" alt="Passionaries Branding Cover">
+					<img src="<?= get_template_directory_uri() ?>/img/image3.jpg" alt="Passionaries Branding Cover">
 					<h4>Passionaries Branding Cover</h4>
 					<h5>Branding, Graphic Design</h5>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
