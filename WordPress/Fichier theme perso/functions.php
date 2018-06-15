b2fbf8471c219ab62fdbe8c3c51f2f507331d073
Piotezaza <mazarine.h@gmail.com>
2018-06-15 10:00:02 +0200

    Update

--- WordPress/Fichier theme perso/functions.php
+++ WordPress/Fichier theme perso/functions.php
@@ -9,7 +9,7 @@ function wpdocs_theme_name_scripts()
 
     wp_enqueue_style( 'normalize', get_template_directory_uri() . '/css/normalize.css' );
     wp_enqueue_style( 'google-font', 'https://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed:400,700|Roboto+Slab:400,700' );
-    wp_enqueue_style( 'main-styles', get_template_directory_uri() . '/css/styles.css' );
+    wp_enqueue_style( 'main-styles', get_template_directory_uri() . '/css/styles.css?v=' . echo uniqid() );
     // wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'wpdocs_theme_name_scripts' );
