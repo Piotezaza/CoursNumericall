20d751d101e29f506f9f162b18ac21af8dc8688d
Piotezaza <mazarine.h@gmail.com>
2018-06-18 10:33:52 +0200

    Update

--- WordPress/Fichier theme perso/inc/custom_posts.php
+++ WordPress/Fichier theme perso/inc/custom_posts.php
@@ -42,3 +42,73 @@ function marble_register_my_cpts() {
 }
 
 add_action( 'init', 'marble_register_my_cpts' );
+
+
+
+
+
+
+
+/*
+    Enregistrement et déclaration des taxonomies personnalisées utiles au thème
+    project_type -> Type de projet (hierarchique = catégorie)
+    project_color -> Couleur du projet (non hierarchique = mot clé)
+*/ 
+function marble_register_my_taxes() {
+
+	/**
+	 * Taxonomy: Types de projet.
+	 */
+
+	$labels = array(
+		"name" => __( "Types de projet", "" ),
+		"singular_name" => __( "Type de projet", "" ),
+	);
+
+	$args = array(
+		"label" => __( "Types de projet", "" ),
+		"labels" => $labels,
+		"public" => true,
+		"hierarchical" => true,
+		"label" => "Types de projet",
+		"show_ui" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"query_var" => true,
+		"rewrite" => array( 'slug' => 'project_type', 'with_front' => true, ),
+		"show_admin_column" => true,
+		"show_in_rest" => false,
+		"rest_base" => "project_type",
+		"show_in_quick_edit" => false,
+	);
+	register_taxonomy( "project_type", array( "project" ), $args );
+
+	/**
+	 * Taxonomy: Couleurs du projet.
+	 */
+
+	$labels = array(
+		"name" => __( "Couleurs du projet", "" ),
+		"singular_name" => __( "Couleur du projet", "" ),
+	);
+
+	$args = array(
+		"label" => __( "Couleurs du projet", "" ),
+		"labels" => $labels,
+		"public" => true,
+		"hierarchical" => false,
+		"label" => "Couleurs du projet",
+		"show_ui" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"query_var" => true,
+		"rewrite" => array( 'slug' => 'project_color', 'with_front' => true, ),
+		"show_admin_column" => true,
+		"show_in_rest" => false,
+		"rest_base" => "project_color",
+		"show_in_quick_edit" => false,
+	);
+	register_taxonomy( "project_color", array( "project" ), $args );
+}
+
+add_action( 'init', 'marble_register_my_taxes' );
