6a4cadbf9ab849fa645bf43bbc43dbeb50193b5b
Piotezaza <mazarine.h@gmail.com>
2018-06-19 16:26:49 +0200

    Update

--- AJAX/Exemple-5/src/utils.php
+++ AJAX/Exemple-5/src/utils.php
@@ -6,4 +6,6 @@ function dbConnect()
         PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ];
+
+    return new PDO('sqlite:' . __DIR__ . '../data/data.db');
 }
