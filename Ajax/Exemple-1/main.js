e8c8200d026b598dc042f7d83147dee0c7d5d336
Piotezaza <mazarine.h@gmail.com>
2018-06-19 10:05:56 +0200

    Update

--- Ajax/Exemple-1/main.js
+++ Ajax/Exemple-1/main.js
@@ -1,9 +1,9 @@
-var url = "lastArticle.html";
+var url ="lastArticles.html"; 
 var refreshButton = document.getElementById('refresh');
 var lastNews = document.getElementById('last-news');
 var xhttp = new XMLHttpRequest();
 
-refreshButton.onclick = function() {
+refreshButton.onclick = function(){
     console.log('AJAX');
-    xhttp.open("GET", url);
+    xhttp.open("GET", url)
 };
