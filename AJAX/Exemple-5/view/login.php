cdea2d9f3d640059f2d6642c4f99d1e38dbd9968
Piotezaza <mazarine.h@gmail.com>
2018-06-19 16:04:24 +0200

    Update

--- AJAX/Exemple-5/view/login.php
+++ AJAX/Exemple-5/view/login.php
@@ -24,6 +24,34 @@
         </form>
     </div>
     <div class="col-6">
-
+    <form action="" method="post">
+            <div class="card">
+                <div class="card-header">
+                    <span class="lead">Créer un compte</span>
+                </div>
+                <div class="card-body">
+                    <div class="form-error badge badge-danger"></div>
+                    <div class="form-group">
+                        <label for="username">Nom d'utilisateur</label>
+                        <input type="text" class="form-control" name="username" id="username" placeholder="Nom d'utilisateur">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" name="email" id="email" placeholder="Email">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Mot de passe</label>
+                        <input type="password" class="form-control" name="password" id="password" placeholder="Mot de passe">
+                    </div>
+                    <div class="form-group">
+                        <label for="avatar">Avatar</label>
+                        <input type="file" class="form-control-file" name="avatar" id="avatar">
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-dark">Se connecter</button>
+                </div>
+            </div>
+        </form>
     </div>
 </div>
