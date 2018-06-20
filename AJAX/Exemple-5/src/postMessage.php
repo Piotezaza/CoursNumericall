149b03c77b60235fb2ba2514d4712a1bb93251f1
Piotezaza <mazarine.h@gmail.com>
2018-06-20 15:30:56 +0200

    Update

--- AJAX/Exemple-5/src/postMessage.php
+++ AJAX/Exemple-5/src/postMessage.php
@@ -7,5 +7,15 @@ $pdo = dbConnect();
 
 if($user = getUser())
 {
-    
+    $result = postMessage($pdo, $user, $_POST['message']);
 }
+else
+{
+    $result = array(
+        'success' => false, 
+        'message' => "Vous n'êtes pas connecté"
+    );
+}
+
+header("Content-Type: application/json");
+echo json_encode( $result );
