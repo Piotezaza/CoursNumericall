ab58671ac8065dd321a30cd3e3c06e834a80a473
Piotezaza <mazarine.h@gmail.com>
2018-06-20 16:12:10 +0200

    Update

--- AJAX/Exemple-5/assets/js/messenger.js
+++ AJAX/Exemple-5/assets/js/messenger.js
@@ -4,9 +4,12 @@ $(function()
 
     function postMessage()
     {
+        // $.ajax({ url: '...', method: 'POST', data: $form.serialize()}).done(function(data){})
         $.post('src/postMessage.php', $form.serialize() /* {message: $message.val(), ...} */, function(data){
             console.log(data);
         }, 'json');
+
+        return false;
     }
 
     $form.submit(postMessage);
