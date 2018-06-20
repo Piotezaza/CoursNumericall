ee92bc28c7892058e067330cca4eacea2a4ee893
Piotezaza <mazarine.h@gmail.com>
2018-06-20 15:07:09 +0200

    Update

--- AJAX/Exemple-5/view/messenger.php
+++ AJAX/Exemple-5/view/messenger.php
@@ -1,5 +1,9 @@
-<navr class="navbar bg-dark fixed-top navbar-dark ml-auto">
-    <div class="col"><a name="" id="" class="btn btn-dark" href="#" role="button">Se déconnecter</a></div>
+<nav class="navbar bg-dark text-light fixed-top navbar-dark ml-auto">
+    <span class="lead">Super Messenger</span>
+    <div class="float-right">
+        <span class="mr-4"><i class="fas fa-user"></i><?=$user['username'] ?></span>
+        <div class="col"><a name="" id="" class="btn btn-light" href="#" role="button">Se déconnecter</a></div>
+    </div>
 </nav>
 
 
