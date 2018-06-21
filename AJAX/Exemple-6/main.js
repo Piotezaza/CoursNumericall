e88ed951d112745dbe8d16143919cea6e972b76c
Piotezaza <mazarine.h@gmail.com>
2018-06-21 13:47:07 +0200

    Update

--- AJAX/Exemple-6/main.js
+++ AJAX/Exemple-6/main.js
@@ -1,3 +1,7 @@
 function initMap(){
-    
+    var piennes = {lat: 49.3074446, lng: 5.7676179}
+    var map = new google.maps.Map($('#map')[0], {
+        center : piennes, 
+        zoom: 8
+    });
 }
