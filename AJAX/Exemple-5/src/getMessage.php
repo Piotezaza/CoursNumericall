38b0978593f254b69fd288390d524c6b6fb25b11
Piotezaza <mazarine.h@gmail.com>
2018-06-21 09:55:53 +0200

    Update

--- AJAX/Exemple-5/src/getMessage.php
+++ AJAX/Exemple-5/src/getMessage.php
@@ -9,4 +9,7 @@ $user = getUser();
 $messages = getLastMessages($pdo, $_GET['lastId']);
 
 header("Content-Type: application/json");
-echo json_encode($messages);
+echo json_encode(array(
+    'messages' => $messages,
+    'user' => $user
+));
