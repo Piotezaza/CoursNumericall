0e219e6cfb94e40b1265a8e243889b0ebcbd26aa
Piotezaza <mazarine.h@gmail.com>
2018-06-22 16:08:12 +0200

    Update

--- PHP/POO/Exemple-5/Form/Form.php
+++ PHP/POO/Exemple-5/Form/Form.php
@@ -6,4 +6,52 @@ class Form
     private $method;
     private $action;
     private $attr;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    public function setMethod($method)
+    {
+        $this->method = $method;
+
+        return $this;
+    }
+
+    public function getAction()
+    {
+        return $this->action;
+    }
+
+    public function setAction($action)
+    {
+        $this->action = $action;
+
+        return $this;
+    }
+ 
+    public function getAttr()
+    {
+        return $this->attr;
+    }
+ 
+    public function setAttr($attr)
+    {
+        $this->attr = $attr;
+
+        return $this;
+    }
 }
