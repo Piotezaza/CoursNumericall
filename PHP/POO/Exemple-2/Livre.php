173f78087cc137a244f3bd769d27f5f34e478048
Piotezaza <mazarine.h@gmail.com>
2018-06-22 14:33:24 +0200

    Update

--- PHP/POO/Exemple-2/Livre.php
+++ PHP/POO/Exemple-2/Livre.php
@@ -6,9 +6,10 @@ final class Livre extends Produit
 {
     private $isbn;
 
-    public function __construct($prix, $poids, $sibn)
+    public function __construct($prix, $poids, $isbn)
     {
         $this->setIsbn($isbn);
+        parent::__construct($prix, $poids);
     }
 
     public function getIsbn()
--- PHP/POO/Exemple-2/index.php
+++ PHP/POO/Exemple-2/index.php
@@ -1,8 +1,9 @@
 <?php
 
 require_once('DVD.php');
+require_once('Livre.php');
 
-$monDVD = new DVD();
+$monDVD = new DVD(9.99, 1);
 $monDVD->setPrix(12.99);
 echo $monDVD->getPrix();
 echo '</br>';
@@ -18,3 +19,6 @@ echo '</br>';
 foreach ($monDVD->getActeurs() as $key => $acteur) {
     echo $acteur . '</br>';
 }
+
+$livre = new Livre(9.99, 200, '684684-54');
+echo $livre->getIsbn();
