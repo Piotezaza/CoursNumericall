1fef129c3a5c69d350e3c8776c41bb19df554038
Piotezaza <mazarine.h@gmail.com>
2018-06-22 11:17:00 +0200

    Update

--- PHP/POO/Exemple-1/index.php
+++ PHP/POO/Exemple-1/index.php
@@ -5,6 +5,7 @@ class Article
     // Propriétés
     private $titre;
     private $statut;
+    private static $counter = 0;
     const S_PUBLIC = 1;
     const S_PRIVATE = 0;
 
@@ -13,6 +14,7 @@ class Article
     {
         $this->setTitre($titre);
         $this->statut = self::S_PUBLIC;
+        self::$counter++;
     }
 
     public function getTitre()
@@ -30,10 +32,16 @@ class Article
     {
         return $this->statut == self::S_PUBLIC;
     }
+
+    public static function getCounter()
+    {
+        return self::counter;
+    }
 }
 
-
+echo Article::$counter . ' articles(s) </br>';
 $monArticle = new Article("Article 1");
+echo Article::$counter . ' articles(s) </br>';
 $article2 = new Article("Article 2");
-
-echo $monArticle->isPublic();
+echo Article::$counter . ' articles(s) </br>';
+echo Article::getCounter();
