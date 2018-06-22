3c718c8316db5b24ad6e577af23e68b92a53a49f
Piotezaza <mazarine.h@gmail.com>
2018-06-22 16:19:52 +0200

    Update

--- PHP/POO/Exemple-5/index.php
+++ PHP/POO/Exemple-5/index.php
@@ -4,6 +4,7 @@ require_once('Include/header.php');
 
 require_once('Form/Form.php');
 
-$myform = new Form ('');
+$myform = new Form ("login");
+echo $myform->createView();
 
 require_once('Include/footer.php');
