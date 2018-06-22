c087b1e22d977f24e5a5846d28c9d7041da11e28
Piotezaza <mazarine.h@gmail.com>
2018-06-22 14:53:13 +0200

    Update

--- PHP/POO/Exemple-3/Carre.php
+++ PHP/POO/Exemple-3/Carre.php
@@ -4,5 +4,16 @@ require_once('Forme.php');
 
 final class Carre extends Forme
 {
-    
+    private $cote;
+
+    public function __construct($posX, $posY, $cote)
+    {
+        $this->cote = $cote;
+        parent::__construct($posX, $posY);
+    }
+
+    public function aire()
+    {
+        $this->cote * $this->cote; // pow($this->cote, 2)
+    }
 }
