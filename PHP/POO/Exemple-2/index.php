553263062ff77d97b6eb8a62004021741fe6118e
Piotezaza <mazarine.h@gmail.com>
2018-06-22 12:40:22 +0200

    Update

--- PHP/POO/Exemple-2/index.php
+++ PHP/POO/Exemple-2/index.php
@@ -6,4 +6,10 @@ $monDVD = new DVD();
 $monDVD->setPrix(12.99);
 
 echo $monDVD->getPrix();
+echo '</br>';
+echo $monDVD->getPoids();
 
+$monDVD->setActeurs(array(
+    'Schwarzenegger',
+    'Stallone'
+));
