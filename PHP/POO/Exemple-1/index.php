dd07089737ade473fe3f0086ed80ac32f42cc4c9
Piotezaza <mazarine.h@gmail.com>
2018-06-22 11:38:43 +0200

    Update

--- PHP/POO/Exemple-1/index.php
+++ PHP/POO/Exemple-1/index.php
@@ -1,30 +1,41 @@
-<?php
+<?php 
 
 class Article
 {
-    // Propriétés
-    private $titre;
+    // Propriétés :
+    private $titre = "Titre par défaut - NON dynamique"; 
+    // si on ne set pas $titre a posteriori, $titre aura comme valeur cette string
+    // il n'est pas obligatoire de définir cette propriété, on peut se contenter de la déclarer, 
+    // auquel cas si on crée par la suite une instance sans donner de valeur à $titre, 
+    // $titre sera vide
+
+    private $contenu;
     private $statut;
-    private static $counter = 0;
+
+    public static $counter = 0;
+
+    // Contantes :
     const S_PUBLIC = 1;
     const S_PRIVATE = 0;
 
-    // Constructeur
-    public function __construct($titre)
+    // Contructeur :
+    public function __construct($titre, $contenu)
     {
         $this->setTitre($titre);
         $this->statut = self::S_PUBLIC;
         self::$counter++;
+
+        $this->setContenu($contenu);
     }
 
+    // Méthodes :
     public function getTitre()
     {
         return $this->titre;
     }
 
     public function setTitre($nouveauTitre)
-    {
-        
+    {        
         $this->titre = $nouveauTitre;
     }
 
@@ -33,15 +44,48 @@ class Article
         return $this->statut == self::S_PUBLIC;
     }
 
-    public static function getCounter()
+    public function getCounter() 
     {
-        return self::counter;
+        return self::$counter;
+    }
+
+    // Getter / Setter obtenu avec clic droit sur $contenu :
+    public function getContenu()
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu($contenu)
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    public function getInfos()
+    {
+        return $this->getTitre() . ' ' . $this->getContenu();
     }
 }
 
-echo Article::$counter . ' articles(s) </br>';
-$monArticle = new Article("Article 1");
-echo Article::$counter . ' articles(s) </br>';
-$article2 = new Article("Article 2");
-echo Article::$counter . ' articles(s) </br>';
-echo Article::getCounter();
+
+
+// echo Article::$counter . '< article(s) <br>';
+
+// $monArticle = new Article("Article 1");
+// echo Article::$counter . '< article(s) <br>';
+
+// $article2 = new Article("Article 2");
+// echo Article::$counter . '< article(s) <br>';
+
+// echo Article::getCounter();
+
+$article1 = new Article('Titre', 'Contenu');
+echo $article1->getInfos();
+echo '<br>';
+
+$article1->setTitre('Nouveau titre');
+$article1->setContenu('Nouveau contenu');
+echo $article1->getInfos();
+
+?>
