77e3c2c83b05a79c45ff30a00062afa516944152
Piotezaza <mazarine.h@gmail.com>
2018-06-25 10:07:22 +0200

    Update

--- PHP/POO/Exemple-5/index.php
+++ PHP/POO/Exemple-5/index.php
@@ -4,6 +4,10 @@ require_once('Include/header.php');
 
 require_once('Form/Form.php');
 require_once('Form/TextItem.php');
+require_once('Entity/User.php');
+
+$user = new User("Loic", "Azerty", "ovigne.loic@gmail.com");
+echo $user->getPassword();
 
 $myform = new Form ("login", "POST", "", array("class" => "form", "id" => "login-form"));
 $myform->addItem(new TextItem("username"));
