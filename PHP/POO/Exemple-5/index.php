9d3d84c6cfeb091c94f70ef97c0b25a9d5700f28
Piotezaza <mazarine.h@gmail.com>
2018-06-25 11:30:05 +0200

    Update

--- PHP/POO/Exemple-5/index.php
+++ PHP/POO/Exemple-5/index.php
@@ -6,12 +6,13 @@ require_once('Form/Form.php');
 require_once('Form/TextItem.php');
 require_once('Entity/User.php');
 
-$user = new User("Loic", "Azerty", "ovigne.loic@gmail.com");
-echo $user->getPassword();
+$user = new User("Piote", "Azerty", "superemail@gmail.com");
+// echo $user->getPassword();
 
 $myform = new Form ("login", "POST", "", array("class" => "form", "id" => "login-form"));
 $myform->setData($user);
 $myform->addItem(new TextItem("username", "Nom d'utilisateur"));
+$myform->addItem(new TextItem("email", "Adresse email"));
 
 echo $myform->createView();
 
