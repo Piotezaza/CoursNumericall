c5d3b8564063451eaf379a1947f365ccff4a678d
Piotezaza <mazarine.h@gmail.com>
2018-06-25 09:55:01 +0200

    Update

--- PHP/POO/Exemple-5/Entity/User.php
+++ PHP/POO/Exemple-5/Entity/User.php
@@ -1,2 +1,64 @@
 <?php
 
+/*
+    ID
+    Username
+    Password
+    Email
+*/
+
+class User
+{
+    private $id;
+    private $username;
+    private $password;
+    private $email;
+    
+    public function getId()
+    {
+        return $this->id;
+    }
+     
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+    
+    public function getUsername()
+    {
+        return $this->username;
+    }
+     
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+     
+    public function getPassword()
+    {
+        return $this->password;
+    }
+   
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+     
+    public function getEmail()
+    {
+        return $this->email;
+    }
+    
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
