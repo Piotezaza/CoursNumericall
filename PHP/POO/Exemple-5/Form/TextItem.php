37ec832d0c6539793d60c7be3849f84c4f98835c
Piotezaza <mazarine.h@gmail.com>
2018-06-25 11:47:45 +0200

    Update

--- PHP/POO/Exemple-5/Form/TextItem.php
+++ PHP/POO/Exemple-5/Form/TextItem.php
@@ -7,7 +7,7 @@ class TextItem extends FormItem
     public function createView()
     {
         $html = $this->startView();
-        $html .= '<input trpe="text" class="form-control" name="' . $this->getName() . '" value="' . $this->getValue() . '" />';
+        $html .= '<input type="text" class="form-control" name="' . $this->getName() . '" value="' . $this->getValue() . '" />';
         $html .= $this->endView();
 
         return $html;
