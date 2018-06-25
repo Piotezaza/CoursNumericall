bddacb91f3d86a87eb3ba9d54fdb88b69fefa878
Piotezaza <mazarine.h@gmail.com>
2018-06-25 10:01:02 +0200

    Update

--- PHP/POO/Exemple-5/Entity/User.php
+++ PHP/POO/Exemple-5/Entity/User.php
@@ -13,51 +13,66 @@ class User
     private $username;
     private $password;
     private $email;
+
+    public function __construct($username, $password, $email)
+    {
+        $this -> setUsername($username);
+        $this -> setPassword($password);
+        $this -> setEmail($email);
+        $this -> id = uniqid();
+    }
     
     public function getId()
     {
-        return $this->id;
+        return $this -> id;
     }
      
     public function setId($id)
     {
-        $this->id = $id;
+        $this -> id = $id;
 
         return $this;
     }
     
     public function getUsername()
     {
-        return $this->username;
+        return $this -> username;
     }
      
     public function setUsername($username)
     {
-        $this->username = $username;
+        $this -> username = $username;
 
         return $this;
     }
      
     public function getPassword()
     {
-        return $this->password;
+        return $this -> password;
     }
    
     public function setPassword($password)
     {
-        $this->password = $password;
+        $this -> password = md5($password);
 
         return $this;
     }
      
     public function getEmail()
     {
-        return $this->email;
+        return $this -> email;
     }
     
     public function setEmail($email)
     {
-        $this->email = $email;
+        if(filter_var($email, FILTER_VALIDATE_EMAIL))
+        {
+            $this -> email = $email;
+        }
+        else
+        {
+            trigger_error("L'adresse email n'esty pas valide");
+        }
 
         return $this;
     }
