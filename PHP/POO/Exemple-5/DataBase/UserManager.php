b7bc1c1dfef95497fce141e29391e24e259a1c79
Piotezaza <mazarine.h@gmail.com>
2018-06-26 16:42:03 +0200

    Update

--- PHP/POO/Exemple-5/DataBase/UserManager.php
+++ PHP/POO/Exemple-5/DataBase/UserManager.php
@@ -8,6 +8,13 @@ class UserManager extends DBManager
 {
     public function save(User $user)
     {
-
+        $query = $this->pdo->prepare("INSERT INTO user (username, password, email, pays, sexe, presentation) VALUES (:username, :password, :email, :pays, :sexe, :presentation)");
+        $query->bindValue(':username', $user->getUsername());
+        $query->bindValue(':password', $user->getPassword());
+        $query->bindValue(':email', $user->getEmail());
+        $query->bindValue(':pays', $user->getPays());
+        $query->bindValue(':sexe', $user->getSexe());
+        $query->bindValue(':presentation', $user->getPresentation());
+        
     }
 }
