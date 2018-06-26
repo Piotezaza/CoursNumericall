f6332363a03078b42b3663bd7c430dbf8db4b208
Piotezaza <mazarine.h@gmail.com>
2018-06-26 12:27:51 +0200

    Update

--- PHP/POO/Exemple-5/Form/TextareaItem.php
+++ PHP/POO/Exemple-5/Form/TextareaItem.php
@@ -9,7 +9,7 @@ class TextareaItem extends FormItem
     public function createView()
     {
         $html = $this->startView();
-        $html .= '<textarea class="form-control" name="' . $this->getName() . '" value="' . $this->getValue() . '" rows="3"></textarea>';
+        $html .= '<textarea class="form-control" name="' . $this->getName() . '" value="' . $this->getValue() . '" rows="3" placeholder="' . $this->getLabel() . '"></textarea>';
         $html .= $this->endView();
 
         return $html;
