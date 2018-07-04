bb5f6d22cd3ac1fae0af5dd5387f24a2f538c735
Piotezaza <mazarine.h@gmail.com>
2018-07-04 16:24:45 +0200

    Update

--- PHP/SYMFONY/Exemple1/src/Controller/Admin/ArticleController.php
+++ PHP/SYMFONY/Exemple1/src/Controller/Admin/ArticleController.php
@@ -36,25 +36,25 @@ class ArticleController extends Controller
 		$form = $this -> createForm(ArticleType::class, $article);
 		$form -> handleRequest($request);
 
-		if(!empty($_POST))
-		{
-			$post = array();
+		// if(!empty($_POST))
+		// {
+		// 	$post = array();
 			
-			foreach($_POST as $key => $p)
-			{
-				$post[$key] = trim($p); // trim enlève les espaces avant & après
-			}
+		// 	foreach($_POST as $key => $p)
+		// 	{
+		// 		$post[$key] = trim($p); // trim enlève les espaces avant & après
+		// 	}
 
-			// HYDRATATION
-			$article 	-> setTitle(strip_tags($post/* ou $_POST si la boucle n'existe pas */['title']))
-						-> setContent($post['content'])
-						;
+		// 	// HYDRATATION
+		// 	$article 	-> setTitle(strip_tags($post/* ou $_POST si la boucle n'existe pas */['title']))
+		// 				-> setContent($post['content'])
+		// 				;
 
-			// SAUVEGARDE DANS LA BDD
-			$em = $this -> getDoctrine() -> getManager();
-			$em -> persist($article);
-			$em -> flush();
-		}
+		// 	// SAUVEGARDE DANS LA BDD
+		// 	$em = $this -> getDoctrine() -> getManager();
+		// 	$em -> persist($article);
+		// 	$em -> flush();
+		// }
 
 		return $this->render('admin/article/new.html.twig', array(
 			'form' => $form->createView(),
