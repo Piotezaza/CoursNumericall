157560c9f72c8960c92dfeba9994d5565b1e9150
Piotezaza <mazarine.h@gmail.com>
2018-07-04 14:11:16 +0200

    Update

--- PHP/SYMFONY/Exemple1/src/Controller/Admin/ArticleController.php
+++ PHP/SYMFONY/Exemple1/src/Controller/Admin/ArticleController.php
@@ -5,13 +5,25 @@ namespace App\Controller\Admin;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/admin/article")
+ */
+
 class ArticleController extends Controller
 {
 	/**
-	 * @Route("/admin/article/")
+	 * @Route("/")
 	 */
 	public function index()
 	{
 		return $this->render('admin/article/index.html.twig');
+	}
+
+	/**
+	 * @Route("/new")
+	 */
+	public function new()
+	{
+		return $this->render('admin/article/new.html.twig');
 	}
 }
