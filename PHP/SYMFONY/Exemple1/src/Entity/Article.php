98fb573e07ac6d86b567282f4b51e5438873eddf
Piotezaza <mazarine.h@gmail.com>
2018-07-04 14:33:03 +0200

    Update

--- PHP/SYMFONY/Exemple1/src/Entity/Article.php
+++ PHP/SYMFONY/Exemple1/src/Entity/Article.php
@@ -14,8 +14,93 @@ class Article
     private $title;
     private $content;
     private $dateCreate;
-    private $dateUpdate;git add *
-    git commit -m "Update"
-    git push origin master
-    
+    private $dateUpdate;
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of dateUpdate
+     */ 
+    public function getDateUpdate()
+    {
+        return $this->dateUpdate;
+    }
+
+    /**
+     * Set the value of dateUpdate
+     *
+     * @return  self
+     */ 
+    public function setDateUpdate($dateUpdate)
+    {
+        $this->dateUpdate = $dateUpdate;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of dateCreate
+     */ 
+    public function getDateCreate()
+    {
+        return $this->dateCreate;
+    }
+
+    /**
+     * Set the value of dateCreate
+     *
+     * @return  self
+     */ 
+    public function setDateCreate($dateCreate)
+    {
+        $this->dateCreate = $dateCreate;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of content
+     */ 
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set the value of content
+     *
+     * @return  self
+     */ 
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of title
+     */ 
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set the value of title
+     *
+     * @return  self
+     */ 
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
 }
