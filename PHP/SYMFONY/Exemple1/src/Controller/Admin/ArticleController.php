25f90db03111aa6c4b40e55a160d3644dbbc7188
Piotezaza <mazarine.h@gmail.com>
2018-07-04 15:03:08 +0200

    Update

--- PHP/SYMFONY/Exemple1/src/Controller/Admin/ArticleController.php
+++ PHP/SYMFONY/Exemple1/src/Controller/Admin/ArticleController.php
@@ -36,9 +36,15 @@ class ArticleController extends Controller
 				$post[$key] = trim($p); // trim enlève les espaces avant & après
 			}
 
+			// HYDRATATION
 			$article 	-> setTitle(strip_tags($post/* ou $_POST si la boucle n'existe pas */['title']))
 						-> setContent($post['content'])
 						;
+
+			// SAUVEGARDE DANS LA BDD
+			$em = $this -> getDoctrine() -> getManager();
+			$em -> persist($article);
+			$em -> flush();
 		}
 
 		return $this->render('admin/article/new.html.twig');
