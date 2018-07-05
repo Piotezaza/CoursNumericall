d9b720ebe79d41da781bda8958fe3f6d9564f7e4
Piotezaza <mazarine.h@gmail.com>
2018-07-05 17:01:00 +0200

    Update

--- PHP/SYMFONY/Exemple1/src/Menu/Builder.php
+++ PHP/SYMFONY/Exemple1/src/Menu/Builder.php
@@ -18,6 +18,8 @@ class Builder
         $menu = $this -> factory -> createItem('root');
         $menu -> setChildrenAttribute('class', 'navbar-nav');
         $menu -> addChild('article.article', ['route' => 'app_admin_article_index']);
+        $parent = $menu -> addChild('article.article', ['route' => 'app_admin_article_index']);
+        $parent -> addChild('article.add', ['route' => 'app_admin_article_new']);
         return $menu;
     }
 }
