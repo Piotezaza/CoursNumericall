bb449cc52ef24acacb90183d12c1aa90025cb965
Piotezaza <mazarine.h@gmail.com>
2018-07-05 16:01:44 +0200

    Update

--- PHP/SYMFONY/Exemple1/src/Menu/Builder.php
+++ PHP/SYMFONY/Exemple1/src/Menu/Builder.php
@@ -3,3 +3,19 @@
 namespace App\Menu;
 
 use Knp\Menu\FactoryInterface;
+
+class Builder
+{
+    private $factory;
+
+    public function __construct(FactoryInterface $factory)
+    {
+        $this -> factory = $factory;
+    }
+
+    public function createAdminMenu()
+    {
+        $menu = $this -> factory -> createItem('root');
+        return $menu;
+    }
+}
