948f13f4bd393ae827825220966ea33063d1b286
Piotezaza <mazarine.h@gmail.com>
2018-07-09 09:54:10 +0200

    Update

--- PHP/SYMFONY/Exemple1/src/Entity/Category.php
+++ PHP/SYMFONY/Exemple1/src/Entity/Category.php
@@ -23,7 +23,7 @@ class Category
     private $name;
 
     /**
-     * @ORM\OneToMant(targetEntity="Article")
+     * @ORM\OneToMant(targetEntity="Article", mappedBy="category")
      */
     private $articles;
 
@@ -53,6 +53,30 @@ class Category
     public function setArticles($articles)
     {
         $this->articles = $articles;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     *
+     * @return  string
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @param  string  $name
+     *
+     * @return  self
+     */ 
+    public function setName(string $name)
+    {
+        $this->name = $name;
 
         return $this;
     }
