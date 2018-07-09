de43aa5d2a3a4e75802bde0e76a2bda1ab46557a
Piotezaza <mazarine.h@gmail.com>
2018-07-09 11:34:58 +0200

    Update

--- PHP/SYMFONY/Exemple1/src/Entity/User.php
+++ PHP/SYMFONY/Exemple1/src/Entity/User.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace AppBundle\Entity;
+namespace App\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
- * @ORM\Table(name="fos_user")
+ * @ORM\Table(name="user")
  */
 class User extends BaseUser
 {
