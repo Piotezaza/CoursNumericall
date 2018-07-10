f1ccb7596740ab0d6a430fb9d874c03bc2ec961e
Piotezaza <mazarine.h@gmail.com>
2018-07-10 12:40:02 +0200

    Update

---
+++ PHP/SYMFONY/Exemple1/src/Menu/Builder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Menu;
+
+use Knp\Menu\FactoryInterface;
+
+class Builder
+{
+    private $factory;
+
+    public function __construct(FactoryInterface $factory)
+    {
+        $this -> factory = $factory;
+    }
+
+    public function createAdminMenu()
+    {
+        $menu = $this -> factory -> createItem('root');
+        $menu -> setChildrenAttribute('class', 'navbar-nav');
+
+        $parent = $menu -> addChild('article.article', ['route' => 'app_admin_article_index']);
+        $parent -> addChild('article.list', ['route' => 'app_admin_article_index']);
+        $parent -> addChild('article.add', ['route' => 'app_admin_article_index']);
+
+        $menu -> addChild('category.category', ['route' => 'app_admin_category_index']);
+        return $menu;
+    }
+
+    public function createMainMenu()
+    {
+        $menu = $this -> factory -> createItem('root');
+        $menu -> setChildrenAttribute('class', 'navbar-nav');
+        $menu -> addChild('article.article', ['route' => 'app_article_index']);
+        return $menu;
+    }
+
+    public function createUserMenu()
+    {
+        $menu = $this -> factory -> createItem('root');
+        $menu -> setChildrenAttribute('class', 'navbar-nav');
+        $parent = $menu -> addChild('user', ['uri' => '#']);
+        $parent -> addChild('logout', ['route' => 'fos_user_security_logout']);
+        return $menu;
+    }
+
+
+}
